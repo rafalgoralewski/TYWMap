c581c4cfa013bb2b57f50d5ff4a48a2057a5bede
rafalgoralewski <rafalgoralewski@hotmail.com>
2014-05-17 19:25:10 +0200

    Visual changes

--- TYWMap/MainPageViewModel.cs
+++ TYWMap/MainPageViewModel.cs
@@ -14,11 +14,55 @@ namespace TYWMap
     public class MainPageViewModel
     {
         private string selectedProvince;
+        private string currentYear;
+        private string countryName;
+        private bool isHolyRomanEmpire;
+        private bool isHabsburg;
+        private string rulerName;
+        private string religion;
 
         public string SelectedProvince
         {
             get { return selectedProvince; }
             set { selectedProvince = value; }
         }
+
+        public string CurrentYear
+        {
+            get { return currentYear; }
+            set { currentYear = value; }
+        }
+
+        public string CountryName
+        {
+            get { return countryName; }
+            set { countryName = value; }
+        }
+
+        public bool IsHolyRomanEmpire
+        {
+            get { return isHolyRomanEmpire; }
+            set { isHolyRomanEmpire = value; }
+        }
+
+        public bool IsHabsburg
+        {
+            get { return isHabsburg; }
+            set { isHabsburg = value; }
+        }
+
+        public string RulerName
+        {
+            get { return rulerName; }
+            set { rulerName = value; }
+        }
+
+        public string Religion
+        {
+            get { return religion; }
+            set { religion = value; }
+        }
+
+        
     }
 }
